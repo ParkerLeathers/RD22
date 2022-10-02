4319525ae069ed09bbc77d69e7acb9df76f2ca64
ParkerLeathers <34355556+ParkerLeathers@users.noreply.github.com>
2022-10-02 11:48:04 -0500

    Add files via upload

--- temp.py
+++ temp.py
@@ -10,21 +10,21 @@ def fmin(k):
 def fmax(k):
     return max([(ydata[i] - k) / (xdata[i] - min(xdata)) for i in range(len(xdata)) if xdata[i] - min(xdata) != 0])
 
-#plot mx+b given k, scalar = 2400
+#plot mx+b given k, from min(xdata)
 def lineplot(m, k):
-    plt.plot([min(xdata),1990],[k,m*(1990-min(xdata)) + k])
+    plt.plot([min(xdata),21],[k,m*(21-min(xdata)) + k])
 
 
 
 
 df = pd.read_csv("every-20-rows-natality.csv", low_memory=False)
 
-xdata = range(1969,1989)
+xdata = range(0,20)
 ydata = [0] * 20
 
 for i in range(df.shape[0]):
     ydata[int(df.DT_YEAR[i]) - 1969] += df.AM_BIRTHWEIGHT3[i] == 1
-    
+   
 
 plt.scatter(xdata, ydata)
 #for k in range(min(ydata)+1,max(ydata)):
@@ -44,23 +44,24 @@ plt.ylim(min(ydata),max(ydata))
 def error(m, k):
     total_error = 0
     for i in range(len(xdata)):
+        #print(str(m) + " " + str(k) + " " + str(total_error))
         total_error += (ydata[i] - (m * xdata[i] + k)) ** 2
     total_error / float(len(xdata) * len(ydata))
-    #print(total_error)
     return total_error
 
 minerr = 999999999
 minm = 0
 mink = 0
-for k in range(min(ydata) + 1, max(ydata)/6):
+for k in range(min(ydata) + 1, max(ydata)-5500):
     #print(k)
     #print(fmin(k))
     #print(fmax(k))
-    for m in range(int(fmin(k)), int(fmax(k))-4):
-        if(error(m, k) < minerr):
+    for m in range(int(fmin(k)), int(fmax(k))):
+        if(abs(error(m, k)) < minerr):
             minerr = error(m, k)
             minm = m
             mink = k
+print(minerr)
 print(minm)
 print(mink)
 lineplot(minm,mink)
